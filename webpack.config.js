36faa8b2a1109cfa40fff4b25d8ddd43c60c6deb
Timo534 <heartbeat9505@gmail.com>
2021-05-30 13:46:57 +0800

    指南：代码分离-防止重复

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,33 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: process.env.dev ? 'development' : 'production',
-  entry: {
-    index: {
-      import: './src/index.js',
-      dependOn: 'shared',
+module.exports = (env) => {
+  return {
+    mode: env.dev ? 'development' : 'production',
+    entry: {
+      index: './src/index.js',
+      another: './src/another-module.js',
     },
-    another: {
-      import: './src/another-module.js',
-      dependOn: 'shared',
+    devtool: env.dev ? 'inline-source-map' : false,
+    devServer: {
+      contentBase: './dist',
     },
-    shared: 'lodash',
-  },
-  devtool: process.env.dev ? 'inline-source-map' : false,
-  devServer: {
-    contentBase: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: '代码分离',
-    }),
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    publicPath: '/',
-  },
-  optimization: {
-    runtimeChunk: 'single',
-    minimize: false,
-  },
-};
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: '代码分离',
+      }),
+    ],
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+      publicPath: '/',
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      },
+      minimize: !env.closeMin,
+    },
+  };
+}
