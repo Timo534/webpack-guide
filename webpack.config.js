92d7338a98dcd96a593d108a08302b51a01d6300
Timo534 <heartbeat9505@gmail.com>
2021-05-30 13:22:07 +0800

    指南：代码分离-入口起点

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,25 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.dev ? 'development' : 'production',
   entry: {
-    index: './src/index.js',
-    print: './src/print.js',
+    index: {
+      import: './src/index.js',
+      dependOn: 'shared',
+    },
+    another: {
+      import: './src/another-module.js',
+      dependOn: 'shared',
+    },
+    shared: 'lodash',
   },
-  devtool: 'inline-source-map',
+  devtool: process.env.dev ? 'inline-source-map' : false,
   devServer: {
     contentBase: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: '开发环境',
+      title: '代码分离',
     }),
   ],
   output: {
@@ -21,5 +28,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
     publicPath: '/',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    minimize: false,
   },
 };
